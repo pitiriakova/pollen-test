e09b86a49d4bdc3c69a2a297ceb86b376978e83f
epitiryakova <kovchar@softserveinc.com>
2022-01-26 22:57:42 +0100

    initial commit

---
+++ src/Pages/Task/Task.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Task = () => <h1>Empty Task</h1>;
+
+Task.propTypes = {};
+
+export default Task;
---
+++ src/Pages/Task/index.js
@@ -0,0 +1,4 @@
+import withTaskQuery from './Task.query.jsx';
+import Task from './Task.jsx';
+
+export default withTaskQuery(Task);
